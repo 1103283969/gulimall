bfb3848d15c4e2378c5e7ecbef1868a33c3ee8ad
wzl <1920731406@qq.com>
2022-10-30 23:12:57 +0800

    CRUD

--- gulimall-coupon/src/main/java/com/atguigu/gulimall/coupon/controller/CouponController.java
+++ gulimall-coupon/src/main/java/com/atguigu/gulimall/coupon/controller/CouponController.java
@@ -5,6 +5,8 @@ import java.util.Map;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,11 +27,25 @@ import com.atguigu.common.utils.R;
  * @email 1920731406qq.com
  * @date 2022-10-30 13:46:03
  */
+
+@RefreshScope
 @RestController
 @RequestMapping("coupon/coupon")
 public class CouponController {
     @Autowired
     private CouponService couponService;
+
+    @Value("${coupon.user.name}")
+    private String name;
+    @Value("${coupon.user.age}")
+    private Integer age;
+
+
+    @RequestMapping("/test")
+    public R test(){
+        return R.ok( ).put( "name" , name ).put( "age" ,age);
+    }
+
 
     @RequestMapping("/member/list")
     public R membercoupons(){
