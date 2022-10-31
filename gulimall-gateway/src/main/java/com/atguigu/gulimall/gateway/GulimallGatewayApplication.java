132003eea5e99110cfd2b93f2bc50d0ec53f98c4
wzl <1920731406@qq.com>
2022-10-31 13:19:36 +0800

    CRUD

---
+++ gulimall-gateway/src/main/java/com/atguigu/gulimall/gateway/GulimallGatewayApplication.java
@@ -0,0 +1,19 @@
+package com.atguigu.gulimall.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+/*
+* 开启服务注册与发现
+*
+* */
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class GulimallGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GulimallGatewayApplication.class, args);
+    }
+
+}
